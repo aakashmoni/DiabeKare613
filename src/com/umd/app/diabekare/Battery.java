0a745ed26dd4dba18bfba48e1aa8d286ba590351
aakashmoni <aakashmoni@yahoo.com>
2012-11-27 20:08:58 -0500

    Battery implementation

---
+++ src/com/umd/app/diabekare/Battery.java
@@ -0,0 +1,46 @@
+package com.umd.app.diabekare;
+
+/**
+ * Simple model of a battery that has a 
+ * given lifetime in minutes.
+ */
+public class Battery
+{
+  /**
+   * Remaining life in this battery.
+   */
+  private int life;
+  
+  /**
+   * Constructs a new battery with the given
+   * number of minutes in its initial lifetime.
+   * @param givenLife
+   *    initial life of this battery
+   */
+  public Battery(int givenLife)
+  {
+    life = givenLife;
+  }
+  
+  /**
+   * Simulates using the battery for the
+   * given number of minutes.
+   * @param minutes
+   *   number of minutes the battery is being used
+   */
+  public void use(int minutes)
+  {
+    life = life - minutes;
+  }
+  
+  /**
+   * Determines whether this battery is dead.
+   * @return
+   *    true if the battery is still alive, false
+   *    otherwise
+   */
+  public boolean isAlive()
+  {
+    return life > 0;
+  }
+}
---
+++ src/com/umd/app/diabekare/BatteryTest.java
@@ -0,0 +1,36 @@
+package com.umd.app.diabekare;
+
+public class BatteryTest
+{
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args)
+  {
+    // Make a battery with a life of 10 minutes
+    Battery b = new Battery(10); 
+    System.out.println("should be alive: " + b.isAlive());
+    
+    // Use it for 5 minutes, should still be alive
+    b.use(5);
+    System.out.println("should be alive: " + b.isAlive());
+    
+    // Use it for 20 minutes, should be dead now
+    b.use(20);
+    System.out.println("should be dead: " + b.isAlive());
+    
+    // Make another battery
+    b = new Battery(10); // ten minutes
+    System.out.println("should be alive: " + b.isAlive());
+    
+    // Use it for 9 minutes, should still be alive
+    b.use(9);
+    System.out.println("should be alive: " + b.isAlive());
+
+    // Use it for one minute more, should now be dead
+    b.use(1);
+    System.out.println("should be dead: " + b.isAlive());
+  }
+
+}
